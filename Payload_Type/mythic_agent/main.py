ad513dee1e921006327a4f13ed4fbb6667fd4ed8
MohamedHdidane <mohamed642hdidane@gmail.com>
2025-05-08 22:25:15 +0100

    cmd added

--- Payload_Type/mythic_agent/main.py
+++
@@ -1,6 +0,0 @@
-import mythic_container
-from agent_functions.builder import *
-from agent_functions.shell import *
-from agent_functions.download import *
-
-mythic_container.mythic_service.start_and_run_forever()
